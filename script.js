5b8732f3880c53da9e5417a910c22b78661cc358
Paolo Gera <61483376+pgerajr@users.noreply.github.com>
2020-10-28 16:21:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,18 +13,18 @@ function showList() {
     ol_list.style.display = "none";
 
     if ((localStorage.getItem("my_list") == null) || (localStorage.getItem("my_list") == "undefined")) {
-        hide_Erase_Buttons()
+        hide_Erase_Buttons();
     } else {
 
-        show_Erase_Buttons()
+        show_Erase_Buttons();
 
         for (let item in array) {
 
             li = document.createElement("li");
             li.innerHTML = array[item];
-            ol_list.appendChild(li)
+            ol_list.appendChild(li);
 
-            ol_list.style.display = "inline-block"
+            ol_list.style.display = "inline-block";
         }
     }
 
@@ -48,43 +48,43 @@ function create_list() {
                 showAlert("To start your list, type something first.\nPlease, use the INPUT field to do so.")
             } else {
 
-                li.innerHTML = data
-                ol_list.appendChild(li)
+                li.innerHTML = data;
+                ol_list.appendChild(li);
 
                 var array = data.split(" ");
 
-                let list = localStorage.setItem("my_list", JSON.stringify(array));
-                show_Erase_Buttons()
+                localStorage.setItem("my_list", JSON.stringify(array));
+                show_Erase_Buttons();
             }
 
-            field_content.value = ""
+            field_content.value = "";
 
 
         } else {
 
             if (data == "") {
-                showAlert("The INPUT field cannot be BLANK. =]")
+                showAlert("The INPUT field cannot be BLANK. =]");
             } else {
 
                 // Trazendo o array armazenado na LOCALSTORAGE de volta.
 
-                let load_list = localStorage.getItem("my_list")
-                let list_items = JSON.parse(load_list)
+                let load_list = localStorage.getItem("my_list");
+                let list_items = JSON.parse(load_list);
 
                 list_items.push(data);
 
-                let list = localStorage.setItem("my_list", JSON.stringify(list_items));
-                show_Erase_Buttons()
+                localStorage.setItem("my_list", JSON.stringify(list_items));
+                show_Erase_Buttons();
             }
 
             field_content.value = ""
 
 
         }
-        showList()
+        showList();
 
     } else {
-        showAlert("We're sorry! Your current web browser doesn't support Web Storage.")
+        showAlert("We're sorry! Your current web browser doesn't support Web Storage.");
     }
 }
 
@@ -92,14 +92,14 @@ function create_list() {
 
 function show_Erase_Buttons() {
     let buttons = document.getElementById("erase_btn");
-    buttons.style.display = "inline-block"
+    buttons.style.display = "inline-block";
 }
 
 // Oculta os botões ERASE TASK e ERASE LIST
 
 function hide_Erase_Buttons() {
     let buttons = document.getElementById("erase_btn");
-    buttons.style.display = "none"
+    buttons.style.display = "none";
 }
 
 
@@ -107,24 +107,28 @@ function hide_Erase_Buttons() {
 
 function show_erase_field() {
 
-    let field_label = document.getElementById("task_eraser_label")
-    let field_content = document.getElementById("task_eraser")
-    let erase_btn = document.getElementById("erase_btn")
+    let field_label = document.getElementById("task_eraser_label");
+    let field_content = document.getElementById("task_eraser");
+    let erase_btn = document.getElementById("erase_btn");
 
     if (erase_btn.style.display == "none") {
-        field_label.style.display = "none"
+        field_label.style.display = "none";
         field_content.style.display = "none";
+        btn_erase_task.style.display = "none";
+
     } else {
 
         if (localStorage.getItem("my_list") == null) {
         } else {
 
             if (field_content.style.display == "inline-block") {
-                field_label.style.display = "none"
+                field_label.style.display = "none";
                 field_content.style.display = "none";
+                btn_erase_task.style.display = "none";
             } else {
-                field_label.style.display = "inline-block"
+                field_label.style.display = "inline-block";
                 field_content.style.display = "inline-block";
+                btn_erase_task.style.display = "inline-block";
             }
 
             field_content.value = "";
@@ -135,32 +139,18 @@ function show_erase_field() {
 
 // Apaga tarefa específica escolhida pelo usuário
 
-function erase_task(item) {
+function erase_Task() {
 
     let field_content = document.getElementById("task_eraser");
-    let load_list = localStorage.getItem("my_list");
-    let list_items = JSON.parse(load_list);
 
-    if (item.value == "") {
+    if (field_content.value == "") {
+        showAlert("You need to type a task number.")
     } else {
 
-        if (list_items.length < Number(item.value)) {
-            showAlert("This task doesn't exist. Try Again. =]")
-        } else {
+        show_Decision_Box();
 
-            delete list_items[Number(item.value) - 1];
-            list_items.sort()
-            list_items.pop()
-
-            localStorage.setItem("my_list", JSON.stringify(list_items))
-
-            showList()
-        }
-
-        field_content.value = ""
     }
 }
-
 
 // Apaga a lista por COMPLETO da LOCALSTORAGE
 
@@ -168,9 +158,8 @@ function erase_List() {
 
     localStorage.clear("my_list")
     let list_items = document.getElementById("ol_list");
-    let alert_message = document.getElementById("alert_message")
 
-    alert_message.innerHTML = "The list was erased!"
+    showAlert("The list was erased!")
 
     list_items.style.display = "none"
 
@@ -181,22 +170,57 @@ function erase_List() {
 
 }
 
-// Mostra o Alerta no DIV Alert Message
+// Mostra a caixa de decisão para exclusão de item da lista.
 
-function showAlert(alert_message) {
+function show_Decision_Box() {
+    let confirm_box = document.getElementById("confirm_box");
+    confirm_box.style.display = "block";
+}
 
-    let message = document.getElementById("alert_message")
-    message.innerText = alert_message
+function hide_Decision_Box() {
+    let confirm_box = document.getElementById("confirm_box");
+    confirm_box.style.display = "none";
+}
+
+function confirm_Erase() {
+
+    let field_content = document.getElementById("task_eraser");
+    let load_list = localStorage.getItem("my_list");
+    let list_items = JSON.parse(load_list);
+
+    if (list_items.length < Number(field_content.value)) {
+        showAlert("This task doesn't exist. Try Again. =]");
+    } else {
+        delete list_items[Number(field_content.value) - 1];
+        list_items.sort();
+        list_items.pop();
+
+        localStorage.setItem("my_list", JSON.stringify(list_items));
+    }
+
+    field_content.value = "";
+
+    hide_Decision_Box();
+    showList();
 
 }
 
+// Mostra o Alerta no DIV Alert Message e o limpa após 3 segundos
+
+function showAlert(alert_message) {
+
+    let message = document.getElementById("alert_message");
+    message.innerText = alert_message;
+
+    setTimeout(clearAlert, 5000);
+}
 
 // Limpa os Alertas escritos no DIV Alert Message
 
 function clearAlert() {
 
-    let message = document.getElementById("alert_message")
-    message.innerText = ""
+    let message = document.getElementById("alert_message");
+    message.innerText = "";
 
 }
 
